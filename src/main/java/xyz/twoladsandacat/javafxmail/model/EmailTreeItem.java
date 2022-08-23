ee8f6929ed09bade35b597d7e0d44ef49e89124b
Igor Anikeev <2822739+ianikeev@users.noreply.github.com>
2022-08-23 18:39:22 +0400

    Fetch messages and update folder names with unread messages count

--- src/main/java/xyz/twoladsandacat/javafxmail/model/EmailTreeItem.java
+++ src/main/java/xyz/twoladsandacat/javafxmail/model/EmailTreeItem.java
@@ -1,13 +1,51 @@
 package xyz.twoladsandacat.javafxmail.model;
 
+import jakarta.mail.Flags;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 
 public class EmailTreeItem<String> extends TreeItem<String> {
 
     private String name;
+    private ObservableList<EmailMessage> emailMessages;
+    private int unreadMessagesCount;
 
     public EmailTreeItem(String name) {
         super(name);
         this.name = name;
+        this.emailMessages = FXCollections.observableArrayList();
+    }
+
+    public void addEmail(Message message) throws MessagingException {
+        boolean messageIsRead = message.getFlags().contains(Flags.Flag.SEEN);
+        EmailMessage emailMessage = new EmailMessage(
+                message.getSubject(),
+                message.getFrom()[0].toString(),
+                message.getRecipients(MimeMessage.RecipientType.TO)[0].toString(),
+                message.getSize(),
+                message.getSentDate(),
+                messageIsRead,
+                message
+        );
+        emailMessages.add(emailMessage);
+        if (!messageIsRead) {
+            incrementMessagesCount();
+        }
+    }
+
+    public void incrementMessagesCount() {
+        unreadMessagesCount++;
+    }
+
+    private void updateName() {
+        if (unreadMessagesCount > 0) {
+            this.setValue((String) (name + " (" + unreadMessagesCount + ")"));
+        } else {
+            this.setValue(name);
+        }
     }
 }
