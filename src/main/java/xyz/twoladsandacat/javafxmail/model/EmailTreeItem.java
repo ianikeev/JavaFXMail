cc56265d8ebb60e8064a9ba56813683800704c9a
Igor Anikeev <2822739+ianikeev@users.noreply.github.com>
2022-08-24 16:22:15 +0400

    Hotfix: Folder name update
    Update folder name when updating unread message count
    count

--- src/main/java/xyz/twoladsandacat/javafxmail/model/EmailTreeItem.java
+++ src/main/java/xyz/twoladsandacat/javafxmail/model/EmailTreeItem.java
@@ -43,6 +43,7 @@ public class EmailTreeItem<String> extends TreeItem<String> {
 
     public void incrementMessagesCount() {
         unreadMessagesCount++;
+        updateName();
     }
 
     private void updateName() {
