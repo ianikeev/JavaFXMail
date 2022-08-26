05a2fdf2749093487aa06ca83d0762a57f13c826
Igor Anikeev <2822739+ianikeev@users.noreply.github.com>
2022-08-26 15:10:25 +0400

    Add folder updater service

--- src/main/java/xyz/twoladsandacat/javafxmail/EmailManager.java
+++ src/main/java/xyz/twoladsandacat/javafxmail/EmailManager.java
@@ -1,22 +1,39 @@
 package xyz.twoladsandacat.javafxmail;
 
+import jakarta.mail.Folder;
 import xyz.twoladsandacat.javafxmail.controller.services.FetchFoldersService;
+import xyz.twoladsandacat.javafxmail.controller.services.FolderUpdaterService;
 import xyz.twoladsandacat.javafxmail.model.EmailAccount;
 import xyz.twoladsandacat.javafxmail.model.EmailTreeItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmailManager {
 
     // Folder handling:
+    private FolderUpdaterService folderUpdaterService;
     private final EmailTreeItem<String> foldersRoot = new EmailTreeItem<>("");
 
     public EmailTreeItem<String> getFoldersRoot() {
         return foldersRoot;
     }
 
+    public List<Folder> getFoldersList() {
+        return foldersList;
+    }
+
+    private List<Folder> foldersList = new ArrayList<>();
+
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(foldersList);
+        folderUpdaterService.start();
+    }
+
     public void addEmailAccount(EmailAccount emailAccount) {
         EmailTreeItem<String> treeItem = new EmailTreeItem<>(emailAccount.getAddress());
         foldersRoot.getChildren().add(treeItem);
-        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem);
+        FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, foldersList);
         fetchFoldersService.start();
     }
 }
--- src/main/java/xyz/twoladsandacat/javafxmail/controller/services/FetchFoldersService.java
+++ src/main/java/xyz/twoladsandacat/javafxmail/controller/services/FetchFoldersService.java
@@ -3,18 +3,24 @@ package xyz.twoladsandacat.javafxmail.controller.services;
 import jakarta.mail.Folder;
 import jakarta.mail.MessagingException;
 import jakarta.mail.Store;
+import jakarta.mail.event.MessageCountEvent;
+import jakarta.mail.event.MessageCountListener;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import xyz.twoladsandacat.javafxmail.model.EmailTreeItem;
+
+import java.util.List;
 
 public class FetchFoldersService extends Service<Void> {
 
     private Store store;
     private EmailTreeItem<String> foldersRoot;
+    private List<Folder> foldersList;
 
-    public FetchFoldersService(Store store, EmailTreeItem<String> foldersRoot) {
+    public FetchFoldersService(Store store, EmailTreeItem<String> foldersRoot, List<Folder> foldersList) {
         this.store = store;
         this.foldersRoot = foldersRoot;
+        this.foldersList = foldersList;
     }
 
     @Override
@@ -35,15 +41,31 @@ public class FetchFoldersService extends Service<Void> {
 
     private void handleFolders(Folder[] folders, EmailTreeItem<String> foldersRoot) throws MessagingException {
         for (Folder folder : folders) {
+            foldersList.add(folder);
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<>(folder.getName());
             foldersRoot.getChildren().add(emailTreeItem);
             foldersRoot.setExpanded(true);
             fetchMessagesOnFolder(folder, emailTreeItem);
+            addMessageListenerToFolder(folder, emailTreeItem);
             if (folder.getType() == Folder.HOLDS_FOLDERS) {
                 Folder[] subFolders = folder.list();
                 handleFolders(subFolders, emailTreeItem);
             }
         }
+    }
+
+    private void addMessageListenerToFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
+        folder.addMessageCountListener(new MessageCountListener() {
+            @Override
+            public void messagesAdded(MessageCountEvent e) {
+
+            }
+
+            @Override
+            public void messagesRemoved(MessageCountEvent e) {
+
+            }
+        });
     }
 
     private void fetchMessagesOnFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
