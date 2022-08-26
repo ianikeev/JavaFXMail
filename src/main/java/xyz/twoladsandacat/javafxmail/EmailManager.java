ae6c512a94f8b076cc33c6244a31b77a988e0d8e
Igor Anikeev <2822739+ianikeev@users.noreply.github.com>
2022-08-26 15:38:05 +0400

    Set message status read upon view

--- src/main/java/xyz/twoladsandacat/javafxmail/EmailManager.java
+++ src/main/java/xyz/twoladsandacat/javafxmail/EmailManager.java
@@ -1,15 +1,29 @@
 package xyz.twoladsandacat.javafxmail;
 
+import jakarta.mail.Flags;
 import jakarta.mail.Folder;
+import jakarta.mail.MessagingException;
 import xyz.twoladsandacat.javafxmail.controller.services.FetchFoldersService;
 import xyz.twoladsandacat.javafxmail.controller.services.FolderUpdaterService;
 import xyz.twoladsandacat.javafxmail.model.EmailAccount;
+import xyz.twoladsandacat.javafxmail.model.EmailMessage;
 import xyz.twoladsandacat.javafxmail.model.EmailTreeItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class EmailManager {
+
+    private EmailMessage selectedMessage;
+    private EmailTreeItem<String> selectedFolder;
+
+    public void setSelectedMessage(EmailMessage selectedMessage) {
+        this.selectedMessage = selectedMessage;
+    }
+
+    public void setSelectedFolder(EmailTreeItem<String> selectedFolder) {
+        this.selectedFolder = selectedFolder;
+    }
 
     // Folder handling:
     private FolderUpdaterService folderUpdaterService;
@@ -35,5 +49,15 @@ public class EmailManager {
         foldersRoot.getChildren().add(treeItem);
         FetchFoldersService fetchFoldersService = new FetchFoldersService(emailAccount.getStore(), treeItem, foldersList);
         fetchFoldersService.start();
+    }
+
+    public void setRead() {
+        try {
+            selectedMessage.setRead(true);
+            selectedMessage.getMessage().setFlag(Flags.Flag.SEEN, true);
+            selectedFolder.decrementMessageCount();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/xyz/twoladsandacat/javafxmail/controller/MainWindowController.java
+++ src/main/java/xyz/twoladsandacat/javafxmail/controller/MainWindowController.java
@@ -81,6 +81,10 @@ public class MainWindowController extends BaseController implements Initializabl
         emailsTableView.setOnMouseClicked(event -> {
             EmailMessage emailMessage = emailsTableView.getSelectionModel().getSelectedItem();
             if (emailMessage != null) {
+                emailManager.setSelectedMessage(emailMessage);
+                if(!emailMessage.isRead()){
+                    emailManager.setRead();
+                }
                 messageRendererService.setEmailMessage(emailMessage);
                 messageRendererService.restart();
             }
@@ -116,6 +120,7 @@ public class MainWindowController extends BaseController implements Initializabl
         emailsTreeView.setOnMouseClicked(e -> {
             EmailTreeItem<String> item = (EmailTreeItem<String>) emailsTreeView.getSelectionModel().getSelectedItem();
             if (item != null) {
+                emailManager.setSelectedFolder(item);
                 emailsTableView.setItems(item.getEmailMessages());
             }
         });
